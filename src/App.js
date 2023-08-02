4b24d42dd7dd0f19ae1d3542e0e37990a2ae237f
John Perkins <john.perkins@uktv.co.uk>
2023-08-02 17:40:30 +0100

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import ImageRail from "./components/ImageRail/ImageRail";
+import Profile from "./components/Profile/Profile";
+import "./App.css";
+import { useState } from "react";
+import Modal from "./components/Modal/Modal";
+export default function App() {
+  const railImagesOne = [
+    "kitten-settle.png",
+    "new-kitten.jpeg",
+    "puppies-kittens.jpg",
+    "roar-cat.avif",
+    "yawn-kitty.webp",
+  ];
 
-function App() {
+  const railImagesTwo = [
+    "kitten-settle.png",
+    "new-kitten.jpeg",
+    "puppies-kittens.jpg",
+    "roar-cat.avif",
+    "yawn-kitty.webp",
+  ];
+
+  const railImagesThree = [
+    "kitten-settle.png",
+    "new-kitten.jpeg",
+    "puppies-kittens.jpg",
+    "roar-cat.avif",
+    "yawn-kitty.webp",
+  ];
+
+  const [modalState, setModalState] = useState(false);
+  const [modalImage, setModalImage] = useState("");
+
+  function handleModal(image) {
+    console.log(image);
+    setModalState(true);
+    setModalImage(image);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Profile />
+      {modalState && (
+        <Modal closeModal={() => setModalState()} modalImage={modalImage} />
+      )}
+      <div className="containerr">
+        <ImageRail
+          railNumber={"One"}
+          railImages={railImagesOne}
+          handleModal={handleModal}
+        />
+        <ImageRail
+          railNumber={"Two"}
+          railImages={railImagesTwo}
+          handleModal={handleModal}
+        />
+        <ImageRail
+          railNumber={"Three"}
+          railImages={railImagesThree}
+          handleModal={handleModal}
+        />
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/ImageRail/ImageRail.jsx
@@ -0,0 +1,39 @@
+import { useRef, useEffect, useState } from "react";
+import styles from "./ImageRail.module.scss";
+
+export default function ImageRail({ railNumber, railImages, handleModal }) {
+  const ref = useRef(null);
+  const [containerHeight, setHeight] = useState(100 + "%");
+  const [animationState, setPlay] = useState("paused");
+  useEffect(() => {
+    if (ref.current) {
+      setHeight(ref.current.scrollHeight + "px");
+      setPlay("running");
+    }
+  }, []);
+
+  return (
+    <div
+      className={`${styles.dFlex} ${styles[`rail${railNumber}`]}`}
+      ref={ref}
+      onMouseEnter={() => setPlay("paused")}
+      onMouseLeave={() => setPlay("running")}
+      style={{
+        height: `${containerHeight}`,
+        animationPlayState: animationState,
+      }}
+    >
+      {railImages.map((image) => {
+        return (
+          <img
+            key={`${image}-${railNumber}`}
+            onClick={() => handleModal(image)}
+            className={styles.railImage}
+            src={require(`../../images/${image}`)}
+            alt={image}
+          ></img>
+        );
+      })}
+    </div>
+  );
+}
