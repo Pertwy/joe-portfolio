ae3d4f68458973397cf930fd2e7bb31b9eead77b
John Perkins <john.perkins@uktv.co.uk>
2023-09-17 16:28:55 +0100

    spruce up

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,5 +2,9 @@ import React from "react";
 import styles from "./Header.module.scss";
 
 export default function Header() {
-  return <div className={styles.header}>Header</div>;
+  return (
+    <div className={styles.header}>
+      <p className={styles.text}>Joseph Perkins</p>
+    </div>
+  );
 }
